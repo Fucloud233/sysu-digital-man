d667fd560ed2c2851a31f0fd4d6a8fd6e767250d
Fucloud-arch <270928474@qq.com>
2023-10-24 23:32:00 +0800

    feat: 添加根据BotType选择Bot函数

--- ai_module/enhance/__init__.py
+++ ai_module/enhance/__init__.py
@@ -7,3 +7,16 @@ from dboperator import DBOPT
 # 预加载DBOPT
 if DBOPT.is_empty:
     DBOPT.load_documents('data/wiki_data.csv')
+
+
+from bots.gpt_bot import GPTBot
+from bots.qianfan_bot import QianfanBot
+from bots.bot import BotType
+
+def get_bot(bot_type: BotType):
+    match bot_type:
+        case BotType.GPT:
+            return GPTBot()
+        case BotType.Qianfan:
+            return QianfanBot()
+        case _: raise TypeError('The type of bot "{}" not found!')
