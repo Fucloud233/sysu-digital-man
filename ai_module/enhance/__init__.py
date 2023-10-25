489c008833c1ce740424e6a694bf6522be7e6f15
Fucloud <270928474@qq.com>
2023-10-25 17:21:36 +0800

    feat: 实现千帆模型的调用接口

--- ai_module/enhance/__init__.py
+++ ai_module/enhance/__init__.py
@@ -8,15 +8,23 @@ from dboperator import DBOPT
 if DBOPT.is_empty:
     DBOPT.load_documents('data/wiki_data.csv')
 
-
 from bots.gpt_bot import GPTBot
+# [ignore] 暂时该模型的调用
+# from bots.rwkv_bot import RWKVBot
+from bots.rwkv_api_bot import RWKVApiBot
 from bots.qianfan_bot import QianfanBot
-from bots.bot import BotType
+from bots.type import BotType
 
+# 根据机器人类别返回对象
 def get_bot(bot_type: BotType):
     match bot_type:
         case BotType.GPT:
             return GPTBot()
+        # case BotType.RWKV:
+        #     return RWKVBot()
+        case BotType.RWKVApi:
+            return RWKVApiBot()
         case BotType.Qianfan:
             return QianfanBot()
-        case _: raise TypeError('The type of bot "{}" not found!')
+        case _: 
+            return None
--- ai_module/enhance/bots/qianfan_bot.py
+++ ai_module/enhance/bots/qianfan_bot.py
@@ -2,44 +2,51 @@
 import requests
 import json
 
-from enhance.bots.bot import Bot, BotType
-
+from config import CONFIG
+from bots.bot import Bot
+from bots.type import BotType
 
 class QianfanBot(Bot):
     def __init__(self):
         super().__init__(BotType.Qianfan)
+        # TODO: 需要对这里进行异常处理
+        self.__access_token = self.__get_access_token()
 
     def _call_api(self, prompt: str):
-        url = "https://aip.baidubce.com/oauth/2.0/token?client_id={}&client_secret={}&grant_type=client_credentials" \
-            .format()
-        
+        payload = json.dumps({
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", self.__get_request_url(), headers=headers, data=payload)
+
+        try:
+            return response.json()['result']
+        except KeyError:
+            print(response.text)
+ 
+    def __get_access_token(self):
         payload = json.dumps("")
         headers = {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
         
-        response = requests.request("POST", url, headers=headers, data=payload)
-        
+        response = requests.request("POST", self.__get_auth_url(), headers=headers, data=payload)
         return response.json().get("access_token")
-
-
-def main():
-        
-    url = "https://aip.baidubce.com/oauth/2.0/token?client_id=【API Key】&client_secret=【Secret Key】&grant_type=client_credentials"
+           
+    def __get_auth_url(self):
+        (api_key, api_secret) = CONFIG.api_key(self.type)
+        return "https://aip.baidubce.com/oauth/2.0/token?client_id={}&client_secret={}&grant_type=client_credentials" \
+            .format(api_key, api_secret)
     
-    payload = json.dumps("")
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-    
-    response = requests.request("POST", url, headers=headers, data=payload)
-    
-    return response.json().get("access_token")
-    
-
-if __name__ == '__main__':
-    access_token = main()
-    print(access_token)
+    def __get_request_url(self):
+        return "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/eb-instant?access_token={}" \
+            .format(self.__access_token)
     
